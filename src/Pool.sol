d72b6a0dd0355dde7e41b317461450f00e950f10
Nour Haridy <nourharidy@gmail.com>
2022-12-14 19:46:51 +0200

    fix problems and second iteration

--- src/Pool.sol
+++ src/Pool.sol
@@ -4,237 +4,213 @@ pragma solidity ^0.8.13;
 interface IERC20 {
     function balanceOf(address) external view returns(uint);
     function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
-    function decimals() external view returns (uint8);
     function transfer(address, uint) external returns (bool);
+    function decimals() external view returns (uint8);
 }
 
 interface IFactory {
-    function getFee(address pool) external view returns (address to, uint feeBps);
+    function getFee(address pool) external view returns (address to, uint feeMantissa);
 }
 
 contract Pool {
-    uint constant KINK_BPS = 8000;
-    uint constant MAX_BASE_APR_BPS = 4000;
-    uint constant MAX_JUMP_APR_BPS = 6000;
-    uint constant MAX_UTIL_BPS = 8000;
-    uint constant AUCTION_INTERVAL = 1 hours;
+
+    uint constant MAX_BASE_APR_MANTISSA = 0.4e18;
+    uint constant MAX_JUMP_APR_MANTISSA = 0.6e18;
+    uint constant MAX_UTIL_MANTISSA = 0.8e18;
     IFactory public immutable factory;
     IERC20 public immutable collateralToken;
-    IERC20 public immutable creditToken;
-    uint public immutable collateralRatioBps;
-    uint public immutable creditTokenDecimals;
-    uint public immutable collateralTokenDecimals;
-
-    uint public totalSupply;
-    mapping (address => uint) public balanceOf;
-
-    uint public totalDebt;
+    IERC20 public immutable loanToken;
+    uint public immutable maxCollateralRatioMantissa;
+    uint immutable kinkMantissa;
+    uint public immutable collateralRatioSpeedMantissa; //change in collateral ratio per second
+    uint public lastCollateralRatioMantissa;
     uint public debtSharesSupply;
     mapping (address => uint) public debtSharesBalanceOf;
-    uint public debtLastUpdated;
+    uint public totalDebt;
+    uint public lastAccrueInterestTime;
+    uint public totalSupply;
+    mapping (address => uint) public balanceOf;
+    mapping (address => uint) public collateralBalanceOf;
 
-    uint public totalAuctionsReserves;
-    mapping (uint => uint) public auctionSharesSupplies;
-    mapping (uint => uint) public auctionSharesInitialSupplies;
-    mapping (uint => uint) public auctionCreditReserves;
-    // auctionId => user => shares
-    mapping (uint => mapping(address => uint)) public auctionShares;
-
-    constructor(IERC20 _collateralToken, IERC20 _creditToken, uint _collateralRatioBps){
-        require(_collateralRatioBps > 0);
+    constructor (IERC20 _collateralToken, IERC20 _loanToken, uint _maxCollateralRatioMantissa, uint _kinkMantissa, uint _collateralRatioSpeedMantissa) {
+        require(_maxCollateralRatioMantissa > 0, "Pool: _maxCollateralRatioMantissa too low");
+        require(_maxCollateralRatioMantissa < 1e18, "Pool: _maxCollateralRatioMantissa too high");
         factory = IFactory(msg.sender);
         collateralToken = _collateralToken;
-        creditToken = _creditToken;
-        collateralRatioBps = _collateralRatioBps;
-        uint8 _creditTokenDecimals = creditToken.decimals();
-        uint8 _collateralTokenDecimals = collateralToken.decimals();
-        require(_creditTokenDecimals > 0 && _creditTokenDecimals <= 18);
-        require(_collateralTokenDecimals > 0 && _collateralTokenDecimals <= 18);
-        creditTokenDecimals = _creditTokenDecimals;
-        collateralTokenDecimals = _collateralTokenDecimals;
+        loanToken = _loanToken;
+        maxCollateralRatioMantissa = _maxCollateralRatioMantissa;
+        kinkMantissa = _kinkMantissa;
+        collateralRatioSpeedMantissa = _collateralRatioSpeedMantissa;
+        lastCollateralRatioMantissa = _maxCollateralRatioMantissa;
     }
 
     modifier accrueInterest {
         if (totalDebt > 0) {
-            // totalDebt = 1,000,000; getBorrowRateBps = 1000; seconds = 1 year; interest = 100,000
-            uint interest = totalDebt * getBorrowRateBps() / 10000 * (block.timestamp - debtLastUpdated) / 365 days;
-            (address feeRecipient, uint feeBps) = factory.getFee(address(this));
+            // totalDebt = 1,000,000; getBorrowRateMantissa = 0.1e18; seconds = 1 year; interest = 100,000
+            uint interest = totalDebt * getBorrowRateMantissa() / 1e18 * (block.timestamp - lastAccrueInterestTime) / 365 days;
+            (address feeRecipient, uint feeMantissa) = factory.getFee(address(this));
             totalDebt += interest;
-            if(feeBps > 0) {
-                uint fee = interest * feeBps / 10000;
-                uint creditShares = creditToShares(fee);
-                totalSupply += creditShares;
-                balanceOf[feeRecipient] += creditShares;
-                emit Transfer(address(0), feeRecipient, creditShares);
+            if(feeMantissa > 0) {
+                uint fee = interest * feeMantissa / 1e18;
+                uint loanTokenShares = loanTokenToShares(fee);
+                totalSupply += loanTokenShares;
+                balanceOf[feeRecipient] += loanTokenShares;
+                emit Transfer(address(0), feeRecipient, loanTokenShares);
             }
         }
-        debtLastUpdated = block.timestamp;
+        lastCollateralRatioMantissa = getCollateralRatioMantissa();
+        lastAccrueInterestTime = block.timestamp;
         _;
     }
 
-    function invest(uint amount) external accrueInterest {
+    function getBorrowRateMantissa() public view returns (uint) {
+        uint util = getUtilizationMantissa();
+        if(util <= kinkMantissa) {
+            return util * MAX_BASE_APR_MANTISSA / 1e18;
+        } else {
+            uint normalRate = kinkMantissa * MAX_BASE_APR_MANTISSA / 1e18;
+            uint excessUtil = util - kinkMantissa;
+            return (excessUtil * MAX_JUMP_APR_MANTISSA / 1e18) + normalRate;
+        }
+    }
+
+    function getUtilizationMantissa() public view returns (uint) {
+        uint supplied = getSuppliedLoanTokens();
+        if(supplied == 0) return 0;
+        return totalDebt * 1e18 / getSuppliedLoanTokens();
+    }
+
+    function loanTokenToShares (uint loanTokenAmount) public view returns (uint) {
+        uint supplied = getSuppliedLoanTokens();
+        if(supplied == 0) return loanTokenAmount;
+        return loanTokenAmount * totalSupply / getSuppliedLoanTokens();
+    }
+
+    function getSuppliedLoanTokens () public view returns (uint) {
+        return totalDebt + loanToken.balanceOf(address(this));
+    }
+
+    function getCollateralRatioMantissa() public view returns (uint) {
+        uint util = getUtilizationMantissa();
+        uint _lastAccrueInterestTime = lastAccrueInterestTime;
+        uint _lastCollateralRatioMantissa = lastCollateralRatioMantissa;
+        if(_lastAccrueInterestTime == block.timestamp) return lastCollateralRatioMantissa;
+
+        if(util <= kinkMantissa) {
+            if(_lastCollateralRatioMantissa == maxCollateralRatioMantissa) return _lastCollateralRatioMantissa;
+            uint timeDelta = block.timestamp - _lastAccrueInterestTime;
+            uint change = timeDelta * collateralRatioSpeedMantissa;
+            if(_lastCollateralRatioMantissa + change > maxCollateralRatioMantissa) {
+                return maxCollateralRatioMantissa;
+            } else {
+                return _lastCollateralRatioMantissa + change;
+            }
+        } else {
+            if(_lastCollateralRatioMantissa == 0) return 0;
+            uint timeDelta = block.timestamp - _lastAccrueInterestTime;
+            uint change = timeDelta * collateralRatioSpeedMantissa;
+            if(_lastCollateralRatioMantissa < change) {
+                return 0;
+            } else {
+                return _lastCollateralRatioMantissa - change;
+            }
+        }
+    }
+   
+    function getDebtOf(address user) public view returns (uint) {
+        if (debtSharesSupply == 0) return 0;
+        return debtSharesBalanceOf[user] * totalDebt / debtSharesSupply;
+    }
+
+    function invest(uint amount) external {
         if (totalSupply == 0) {
             totalSupply = amount;
             balanceOf[msg.sender] = amount;
 
             emit Transfer(address(0), msg.sender, amount);
         } else {
-            uint creditShares = creditToShares(amount);
-            totalSupply += creditShares;
-            balanceOf[msg.sender] += creditShares;
+            uint loanTokenShares = loanTokenToShares(amount);
+            totalSupply += loanTokenShares;
+            balanceOf[msg.sender] += loanTokenShares;
 
-            emit Transfer(address(0), msg.sender, creditShares);
+            emit Transfer(address(0), msg.sender, loanTokenShares);
         }
-        
-        creditToken.transferFrom(msg.sender, address(this), amount);
-    }    
 
-    function divest (uint amount) public accrueInterest {
-        uint creditShares = creditToShares(amount);
-        totalSupply -= creditShares;
-        balanceOf[msg.sender] -= creditShares;
-
-        creditToken.transfer(msg.sender, amount);
-        emit Transfer(msg.sender, address(0), creditShares);      
+        loanToken.transferFrom(msg.sender, address(this), amount);
     }
 
-    // If returns auctionId as 0 then no auction
-    function recall (uint amount) external returns (uint auctionId) {
-        if(creditToken.balanceOf(address(this)) >= amount) {
-            divest(amount);
-        } else {
-            uint creditShares = creditToShares(amount);
-            balanceOf[msg.sender] -= creditShares;
-            balanceOf[address(this)] += creditShares;
+    function divest(uint amount) external accrueInterest {
+        uint loanTokenShares = loanTokenToShares(amount);
+        totalSupply -= loanTokenShares;
+        balanceOf[msg.sender] -= loanTokenShares;
 
-            emit Transfer(msg.sender, address(this), creditShares);
+        loanToken.transfer(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), loanTokenShares);
+    }
 
-            auctionId = block.timestamp / AUCTION_INTERVAL + 1;
-            uint auctionSharesSupply = auctionSharesSupplies[auctionId];
-            if(auctionSharesSupply == 0) {
-                auctionSharesInitialSupplies[auctionId] = creditShares;
-                auctionSharesSupplies[auctionId] = creditShares;
-                auctionShares[auctionId][msg.sender] = creditShares;
-            } else {
-                uint shares = creditShares * auctionSharesSupply / (creditShares + auctionSharesSupply);
-                auctionSharesInitialSupplies[auctionId] += shares;
-                auctionSharesSupplies[auctionId] += shares;
-                auctionShares[auctionId][msg.sender] += shares;
-            }
+
+    function secure (uint amount) external {
+        require(amount > 0, "Pool: amount too low");
+        collateralBalanceOf[msg.sender] += amount;
+        collateralToken.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function assertCollateralRatio(address user) internal view {
+        if(getDebtOf(user) > 0) {
+            uint userCollateralRatioMantissa = getDebtOf(user) * 1e18 / collateralBalanceOf[user];
+            require(userCollateralRatioMantissa <= lastCollateralRatioMantissa, "Pool: user collateral ratio too high");
         }
     }
 
-    function sell(uint creditTokenAmount, uint minCollateralAmount) external accrueInterest {
-        uint auctionId = block.timestamp / AUCTION_INTERVAL;
-        // TODO: TEST
-        uint maxCollateralCost = creditTokenAmount * (10 ** (18 - creditTokenDecimals)) * 10000 / collateralRatioBps / (10 ** (18 - collateralTokenDecimals));
-        uint auctionStartTimestamp = auctionId * AUCTION_INTERVAL;
-        uint currentCollateralCost = (block.timestamp - auctionStartTimestamp) * 10000 / AUCTION_INTERVAL * maxCollateralCost / 10000;
-        require(currentCollateralCost >= minCollateralAmount);
-        uint creditShares = creditToShares(creditTokenAmount);
-        // TODO: Test auction cannot oversell collateral for more creditTokens than auctioned
-        auctionSharesSupplies[auctionId] -= creditShares;
-        auctionCreditReserves[auctionId] += creditTokenAmount;
-        totalAuctionsReserves += creditTokenAmount;
-        totalDebt -= creditTokenAmount;
-        totalSupply -= creditShares;
-        balanceOf[address(this)] -= creditShares;
-        emit Transfer(address(this), address(0), creditShares);
-        creditToken.transferFrom(msg.sender, address(this), creditTokenAmount);
-        collateralToken.transfer(msg.sender, currentCollateralCost);
+    function isLiquidatable(address user) public view returns (bool) {
+        if(getDebtOf(user) == 0) return false;
+        uint userCollateralRatioMantissa = getDebtOf(user) * 1e18 / collateralBalanceOf[user];
+        if (userCollateralRatioMantissa > getCollateralRatioMantissa()) return true;
+        return false;
     }
 
-    function claimAuctionProceeds(uint auctionId) external {
-        uint currentAuctionId = block.timestamp / AUCTION_INTERVAL;
-        require(auctionId < currentAuctionId);
-        uint auctionShareBalance = auctionShares[auctionId][msg.sender];
-        uint proceeds = auctionShareBalance * auctionCreditReserves[auctionId] / auctionSharesInitialSupplies[auctionId];
-        uint remainingSharesRefund = auctionShareBalance * auctionSharesSupplies[auctionId] / auctionSharesInitialSupplies[auctionId];
-        auctionShares[auctionId][msg.sender] = 0;
-        auctionSharesInitialSupplies[auctionId] -= auctionShareBalance;
-        auctionCreditReserves[auctionId] -= proceeds;
-        totalAuctionsReserves -= proceeds;
-        if(proceeds > 0) {
-            creditToken.transfer(msg.sender, proceeds);
-        }
-        if(remainingSharesRefund > 0) {
-            balanceOf[address(this)] -= remainingSharesRefund;
-            balanceOf[msg.sender] += remainingSharesRefund;
-            emit Transfer(address(this), msg.sender, remainingSharesRefund);
-        }
+    function unsecure(uint amount) external accrueInterest {
+        require(amount > 0, "Pool: amount too low");
+        collateralBalanceOf[msg.sender] -= amount;
+        collateralToken.transfer(msg.sender, amount);
+        assertCollateralRatio(msg.sender);
     }
 
-    function creditToShares (uint creditTokenAmount) public view returns (uint) {
-        return creditTokenAmount * totalSupply / getSuppliedCreditTokens();
-    }
-
-    function getSuppliedCreditTokens () public view returns (uint) {
-        return totalDebt + creditToken.balanceOf(address(this)) - totalAuctionsReserves;
-    }
-
-    function getDebtOf(address user) public view returns (uint) {
-        if (debtSharesSupply == 0) return 0;
-        return debtSharesBalanceOf[user] * totalDebt / debtSharesSupply;
-    }
-
-    function getInvestmentOf(address user) public view returns (uint) {
-        if (totalSupply == 0) return 0;
-        return balanceOf[user] * getSuppliedCreditTokens() / totalSupply;
-    }
-
-    function getCollateralOf(address user) public view returns (uint) {
-        if (debtSharesSupply == 0) return 0;
-        return debtSharesBalanceOf[user] * collateralToken.balanceOf(address(this)) / debtSharesSupply;
-    }
-
-    function getUtilizationBps() public view returns (uint) {
-        uint _totalDebt = totalDebt;
-        return _totalDebt / getSuppliedCreditTokens() * 10000;
-    }
-
-    function getBorrowRateBps() public view returns (uint) {
-        uint util = getUtilizationBps();
-        if(util <= KINK_BPS) {
-            return util * MAX_BASE_APR_BPS / KINK_BPS;
-        } else {
-            uint excessUtil = util - KINK_BPS;
-            return (excessUtil * MAX_JUMP_APR_BPS / (10000 - KINK_BPS)) + MAX_BASE_APR_BPS;
-        }
-    }
-
-    function borrow (uint amount) external accrueInterest {
-        require(totalDebt + amount / getSuppliedCreditTokens() * 10000 < MAX_UTIL_BPS, "EXCEEDED MAX UTILIZATION");
-        
+    function borrow(uint amount) external accrueInterest {
+        require(amount > 0, "Pool: amount too low");
         if (debtSharesSupply == 0) {
             debtSharesSupply = amount;
             debtSharesBalanceOf[msg.sender] = amount;
         } else {
-            // TODO: test interest accrual over time
             uint debtShares = amount * debtSharesSupply / totalDebt;
             debtSharesSupply += debtShares;
             debtSharesBalanceOf[msg.sender] += debtShares; 
         }
         totalDebt += amount;
-
-        // TODO: TEST
-        uint collateralAmount = amount * (10 ** (18 - creditTokenDecimals)) * 10000 / collateralRatioBps / (10 ** (18 - collateralTokenDecimals));
-        collateralToken.transferFrom(msg.sender, address(this), collateralAmount);
-        creditToken.transfer(msg.sender, amount);        
+        loanToken.transfer(msg.sender, amount);
+        assertCollateralRatio(msg.sender);
     }
 
     function repay(address borrower, uint amount) external accrueInterest {
+        require(amount > 0, "Pool: amount too low");
         uint debtShares = amount * debtSharesSupply / totalDebt;
-        debtSharesBalanceOf[borrower] -= debtShares;
-        // TODO: test interest accrual over time
-        totalDebt -= amount;
         debtSharesSupply -= debtShares;
-
-        uint collateralAmount = amount * (10 ** (18 - creditTokenDecimals)) * 10000 / collateralRatioBps / (10 ** (18 - collateralTokenDecimals));
-        creditToken.transferFrom(msg.sender, address(this), amount);
-        collateralToken.transfer(msg.sender, collateralAmount);
+        debtSharesBalanceOf[borrower] -= debtShares;
+        totalDebt -= amount;
+        loanToken.transferFrom(msg.sender, address(this), amount);
     }
 
-
-
+    function liquidate(address borrower, uint amount) external accrueInterest {
+        require(isLiquidatable(borrower), "Pool: borrower not liquidatable");
+        require(amount > 0, "Pool: amount too low");
+        uint userInvertedCollateralRatioMantissa = collateralBalanceOf[borrower] * 1e18 / getDebtOf(borrower);
+        uint debtShares = amount * debtSharesSupply / totalDebt;
+        debtSharesSupply -= debtShares;
+        debtSharesBalanceOf[borrower] -= debtShares;
+        totalDebt -= amount;
+        uint collateralReward = amount * userInvertedCollateralRatioMantissa / 1e18;
+        collateralBalanceOf[borrower] -= collateralReward;
+        loanToken.transferFrom(msg.sender, address(this), amount);
+        collateralToken.transfer(msg.sender, collateralReward);
+    }
     event Transfer(address from, address to, uint value);
-}
--- test/Pool.t.sol
+++ test/Pool.t.sol
@@ -7,21 +7,9 @@ import "./mocks/ERC20.sol";
 
 contract PoolTest is Test {
 
-    MockERC20 collateralToken;
-    MockERC20 creditToken;
-    Pool public pool;
 
     function setUp() public {
-        collateralToken = new MockERC20(1000e6, 6); // e.g. USDC/USDT
-        creditToken = new MockERC20(1000e18, 18); // e.g. Ether
-        pool = new Pool(
-            IERC20(address(collateralToken)),
-            IERC20(address(creditToken)),
-            10000000 // 10 million bps ratio = 1000x
-        );
-    }
 
-    function testGetMaxCreditOf() public {
     }
 
 }
